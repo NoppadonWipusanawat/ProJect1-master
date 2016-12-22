29e62ac78acf3685a538212b8e7420716d6db131
N56J <guherokill@hotmail.com>
2016-12-22 19:40:49 +0700

    ...User

--- app/src/main/java/com/example/n56j/project1/MainActivityUser.java
+++ app/src/main/java/com/example/n56j/project1/MainActivityUser.java
@@ -33,7 +33,7 @@ public class MainActivityUser extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_activity_user);
 
         editText = (EditText) findViewById(R.id.editText33);
 
